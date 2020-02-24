2b420d98cca9d9a8ae41d88b8adf873b627d1732
Adr1anZarza <9055350+adr1anzarza@users.noreply.github.com>
2020-02-24 15:46:38 -0600

    Navigation drawer added to the project

--- andfun-kotlin-android-trivia-Step.07.Exercise-Adding-Safe-Arguments/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ andfun-kotlin-android-trivia-Step.07.Exercise-Adding-Safe-Arguments/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -46,8 +46,40 @@ class GameWonFragment : Fragment() {
             view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
 
-        val args = GameWonFragmentArgs.fromBundle(arguments!!)
-        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    // Creating our Share Intent
+    private fun getShareIntent(): Intent {
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        return ShareCompat.IntentBuilder.from(activity!!)
+                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+                .setType("text/plain")
+                .intent
+
+    }
+
+    // Starting an Activity with our new Intent
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.winner_menu,menu)
+        //Checking if the activity resolves
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)){
+            //hiding the menu item if it doesn't resolve
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- andfun-kotlin-android-trivia-Step.07.Exercise-Adding-Safe-Arguments/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ andfun-kotlin-android-trivia-Step.07.Exercise-Adding-Safe-Arguments/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -17,15 +17,9 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
